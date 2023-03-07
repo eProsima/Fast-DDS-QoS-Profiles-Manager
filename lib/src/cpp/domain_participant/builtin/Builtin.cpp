00b59c3cd921dfd84e4997343f4ff9f8992ccd16
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2023-03-07 16:35:27 +0100

    Refs #17485: DomainParticipant::builtin general module
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

---
+++ lib/src/cpp/domain_participant/builtin/Builtin.cpp
@@ -0,0 +1,202 @@
+// Copyright 2023 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file
+ */
+
+#include <domain_participant/builtin/Builtin.hpp>
+
+#include <string>
+
+#include <exception/Exception.hpp>
+
+namespace eprosima {
+namespace qosprof {
+namespace domain_participant {
+namespace builtin {
+
+std::string print(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_avoid_builtin_multicast(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_use_writer_liveliness_protocol(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_reader_history_memory_policy(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_writer_history_memory_policy(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_reader_payload_size(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_writer_payload_size(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_mutation_tries(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_avoid_builtin_multicast(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_use_writer_liveliness_protocol(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_reader_history_memory_policy(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_writer_history_memory_policy(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_reader_payload_size(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+
+void clear_witer_payload_size(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_mutation_tries(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_avoid_builtin_multicast(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& avoid_builtin_multicast)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_use_writer_liveliness_protocol(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& use_writer_liveliness_protocol)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_reader_history_memory_policy(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& reader_history_memory_policy)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_writer_history_memory_policy(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& writer_history_memory_policy)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_reader_payload_size(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& reader_payload_size)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_writer_payload_size(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& writer_payload_size)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_mutation_tries(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& mutation_tries)
+{
+    throw Unsupported("Unsupported");
+}
+
+} // builtin
+} // domain_participant
+} // qosprof
+} // builtin
