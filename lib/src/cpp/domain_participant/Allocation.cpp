8fdcc7b9c8176ebdf9d7028878078db482cdeff6
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2023-03-09 16:00:06 +0100

    Refs #17485: DomainParticipant::Allocations submodule
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

---
+++ lib/src/cpp/domain_participant/Allocation.cpp
@@ -0,0 +1,469 @@
+// Copyright 2023 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file
+ */
+
+#include <domain_participant/Allocation.hpp>
+
+#include <string>
+
+#include <exception/Exception.hpp>
+
+namespace eprosima {
+namespace qosprof {
+namespace domain_participant {
+namespace allocations {
+
+std::string print(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_remote_locators(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_remote_locators_max_unicast(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_remote_locators_max_multicast(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_total_participants(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_total_participants_initial(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_total_participants_maximum(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_total_participants_increment(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_total_readers(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_total_readers_initial(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_total_readers_maximum(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_total_readers_increment(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_total_writers(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_total_writers_initial(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_total_writers_maximum(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_total_writers_increment(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_max_partitions(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_max_user_data(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_max_properties(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_send_buffers(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_send_buffers_preallocated_number(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_send_buffers_dynamic(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_remote_locators(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_remote_locators_max_unicast(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_remote_locators_max_multicast(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_total_participants(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_total_participants_initial(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_total_participants_maximum(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_total_participants_increment(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_total_readers(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_total_readers_initial(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_total_readers_maximum(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_total_readers_increment(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_total_writers(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_total_writers_initial(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_total_writers_maximum(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_total_writers_increment(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_max_partitions(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_max_user_data(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_max_properties(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_send_buffers(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_send_buffers_preallocated_number(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_send_buffers_preallocated_dynamic(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_remote_locators_max_unicast(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& max_unicast)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_remote_locators_max_multicast(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& max_multicast)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_total_participants_initial(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& initial)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_total_participants_maximum(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& maximum)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_total_participants_increment(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& increment)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_total_readers_initial(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& initial)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_total_readers_maximum(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& maximum)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_total_readers_increment(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& increment)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_total_writers_initial(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& initial)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_total_writers_maximum(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& maximum)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_total_writers_increment(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& increment)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_max_partitions(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& max_partitions)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_max_user_data(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& max_user_data)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_max_properties(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& max_properties)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_send_buffers_preallocated_number(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& send_buffers_preallocated_number)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_send_buffers_dynamic(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& send_buffers_dynamic)
+{
+    throw Unsupported("Unsupported");
+}
+
+} // allocations
+} // domain_participant
+} // qosprof
+} // eprosima
