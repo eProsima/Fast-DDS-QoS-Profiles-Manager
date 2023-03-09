293bb52e15448010a331fc57840e299420048ff3
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2023-03-09 12:02:28 +0100

    Refs #17485: DomainParticipant::Port sunmodule
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

---
+++ lib/src/cpp/domain_participant/Port.cpp
@@ -0,0 +1,202 @@
+// Copyright 2023 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file
+ */
+
+#include <domain_participant/Port.hpp>
+
+#include <string>
+
+#include <exception/Exception.hpp>
+
+namespace eprosima {
+namespace qosprof {
+namespace domain_participant {
+namespace port {
+
+std::string print(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_base(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_domain_id_gain(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_participant_id_gain(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_offset_d0(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_offset_d1(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_offset_d2(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_offset_d3(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_base(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_domain_id_gain(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_participant_id_gain(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_offset_d0(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_offset_d1(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_offset_d2(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_offset_d3(
+        const std::string& xml_file,
+        const std::string& profile_id)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_base(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& port_base)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_domain_id_gain(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& domain_id_gain)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_participant_id_gain(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& domain_id_gain)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_offset_d0(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& offset_d0)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_offset_d1(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& offset_d1)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_offset_d2(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& offset_d2)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_offset_d3(
+        const std::string& xml_file,
+        const std::string& profile_id,
+        const std::string& offset_d3)
+{
+    throw Unsupported("Unsupported");
+}
+
+} // port
+} // domain_participant
+} // qosprof
+} // eprosima
+
