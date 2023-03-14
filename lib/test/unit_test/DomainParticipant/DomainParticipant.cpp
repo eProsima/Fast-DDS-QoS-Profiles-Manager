8ddd1b685f881cc68908225a391fc252f51b22fd
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2023-03-14 14:49:52 +0100

    Refs #17603: domain participant default profile test
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

--- lib/test/unit_test/DomainParticipant/DomainParticipant.cpp
+++ lib/test/unit_test/DomainParticipant/DomainParticipant.cpp
@@ -12,9 +12,70 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <cstdio>
+#include <string>
+
 #include "gtest/gtest.h"
 
 #include <domain_participant/DomainParticipant.hpp>
+#include <exception/Exception.hpp>
+
+using namespace eprosima::qosprof;
+using namespace eprosima::qosprof::domain_participant;
+
+TEST(DomainParticipant, default_profile_test)
+{
+    // Test variables
+    std::string xml_filename = "test.xml";
+    std::string non_existent_xml = "non_existent.xml";
+    std::string participant_profile = "test_profile";
+    std::string another_participant_profile = "second_test_profile";
+    std::string participant_name = "participant_test";
+    std::string another_participant_name = "second_participant_test";
+
+    // Try printing default profile for non-existing file
+    EXPECT_THROW(print_default_profile(xml_filename), FileNotFound);
+
+    // Set participant name in order to create file
+    EXPECT_NO_THROW(set_name(xml_filename, participant_profile, participant_name));
+
+    // Print non-existing default profile
+    EXPECT_THROW(print_default_profile(xml_filename), ElementNotFound);
+
+    // Set default profile to non-existent XML file
+    EXPECT_THROW(set_default_profile(non_existent_xml, participant_profile), FileNotFound);
+
+    // Set default profile to non-existent participant profile
+    EXPECT_THROW(set_default_profile(xml_filename, another_participant_profile), ElementNotFound);
+
+    // Set default profile to existent profile
+    EXPECT_NO_THROW(set_default_profile(xml_filename, participant_profile));
+
+    // Print existent default profile
+    EXPECT_EQ(print_default_profile(xml_filename), participant_profile);
+
+    // Create a second profile and change default profile to this second profile
+    EXPECT_NO_THROW(set_name(xml_filename, another_participant_profile, another_participant_name));
+    EXPECT_NO_THROW(set_default_profile(xml_filename, another_participant_profile));
+
+    // Print existent default profile
+    EXPECT_EQ(print_default_profile(xml_filename), another_participant_profile);
+
+    // Clear default profile
+    EXPECT_NO_THROW(clear_default_profile(xml_filename));
+
+    // Print non-existent default profile
+    EXPECT_THROW(print_default_profile(xml_filename), ElementNotFound);
+
+    // Clear default profile from a file with no default profile
+    EXPECT_NO_THROW(clear_default_profile(xml_filename));
+
+    // Clear default profile from a non-existent XML file
+    EXPECT_THROW(clear_default_profile(non_existent_xml), FileNotFound);
+
+    // Clean test workspace
+    std::remove(xml_filename.c_str());
+}
 
 int main(
         int argc,
