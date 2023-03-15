ce3ca65dae4c2bf4e5075636e2480cc76b79066f
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2023-03-15 16:31:47 +0100

    Refs #17603: complete default external unicast locator tests (with exception to generic print/clear methods)
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

--- lib/test/unit_test/DomainParticipant/DomainParticipant.cpp
+++ lib/test/unit_test/DomainParticipant/DomainParticipant.cpp
@@ -389,7 +389,142 @@ TEST_F(DomainParticipantTests, default_external_unicast_locators_port)
 
     // Call test
     print_push_update_clear_test();
+}
 
+TEST_F(DomainParticipantTests, default_external_unicast_locators_address)
+{
+    // Test variables
+    valid_values_.push_back("localhost");
+    valid_values_.push_back("127.0.0.1");
+    invalid_values_.push_back("");
+
+    // Initialize functors
+    print_functor_ = [](const std::string& xml_file, const std::string& profile_id, int32_t index) -> std::string
+            {
+                return default_external_unicast_locators::print_address(xml_file, profile_id, index);
+            };
+    push_functor_ = [](const std::string& xml_file, const std::string& profile_id, const std::string& kind) -> void
+            {
+                default_external_unicast_locators::push_address(xml_file, profile_id, kind);
+            };
+    update_functor_ = [](const std::string& xml_file, const std::string& profile_id, const std::string& kind,
+                    int32_t index) -> void
+            {
+                default_external_unicast_locators::update_address(xml_file, profile_id, kind, index);
+            };
+    clear_functor_ = [](const std::string& xml_file, const std::string& profile_id, int32_t index) -> void
+            {
+                default_external_unicast_locators::clear_address(xml_file, profile_id, index);
+            };
+
+    // Call test
+    print_push_update_clear_test();
+}
+
+TEST_F(DomainParticipantTests, default_external_unicast_locators_externality)
+{
+    // Test variables
+    valid_values_.push_back("1");
+    valid_values_.push_back("126");
+    invalid_values_.push_back("invalid");
+    invalid_values_.push_back("256");
+    invalid_values_.push_back("0");
+    invalid_messages_.push_back("does not match regular expression facet");
+    invalid_messages_.push_back("must be less than or equal to maxInclusive facet value");
+    invalid_messages_.push_back("must be greater than or equal to minInclusive facet value");
+
+    // Initialize functors
+    print_functor_ = [](const std::string& xml_file, const std::string& profile_id, int32_t index) -> std::string
+            {
+                return default_external_unicast_locators::print_externality(xml_file, profile_id, index);
+            };
+    push_functor_ = [](const std::string& xml_file, const std::string& profile_id, const std::string& kind) -> void
+            {
+                default_external_unicast_locators::push_externality(xml_file, profile_id, kind);
+            };
+    update_functor_ = [](const std::string& xml_file, const std::string& profile_id, const std::string& kind,
+                    int32_t index) -> void
+            {
+                default_external_unicast_locators::update_externality(xml_file, profile_id, kind, index);
+            };
+    clear_functor_ = [](const std::string& xml_file, const std::string& profile_id, int32_t index) -> void
+            {
+                default_external_unicast_locators::clear_externality(xml_file, profile_id, index);
+            };
+
+    // Call test
+    print_push_update_clear_test();
+}
+
+TEST_F(DomainParticipantTests, default_external_unicast_locators_cost)
+{
+    // Test variables
+    valid_values_.push_back("0");
+    valid_values_.push_back("126");
+    invalid_values_.push_back("invalid");
+    invalid_values_.push_back("256");
+    invalid_values_.push_back("-1");
+    invalid_messages_.push_back("does not match regular expression facet");
+    invalid_messages_.push_back("must be less than or equal to maxInclusive facet value");
+    invalid_messages_.push_back("must be greater than or equal to minInclusive facet value");
+
+    // Initialize functors
+    print_functor_ = [](const std::string& xml_file, const std::string& profile_id, int32_t index) -> std::string
+            {
+                return default_external_unicast_locators::print_cost(xml_file, profile_id, index);
+            };
+    push_functor_ = [](const std::string& xml_file, const std::string& profile_id, const std::string& kind) -> void
+            {
+                default_external_unicast_locators::push_cost(xml_file, profile_id, kind);
+            };
+    update_functor_ = [](const std::string& xml_file, const std::string& profile_id, const std::string& kind,
+                    int32_t index) -> void
+            {
+                default_external_unicast_locators::update_cost(xml_file, profile_id, kind, index);
+            };
+    clear_functor_ = [](const std::string& xml_file, const std::string& profile_id, int32_t index) -> void
+            {
+                default_external_unicast_locators::clear_cost(xml_file, profile_id, index);
+            };
+
+    // Call test
+    print_push_update_clear_test();
+}
+
+TEST_F(DomainParticipantTests, default_external_unicast_locators_mask)
+{
+    // Test variables
+    // Default: UDP v4
+    valid_values_.push_back("1");
+    valid_values_.push_back("24");
+    invalid_values_.push_back("invalid");
+    invalid_values_.push_back("32");
+    invalid_values_.push_back("0");
+    invalid_messages_.push_back("does not match regular expression facet");
+    invalid_messages_.push_back("must be less than or equal to maxInclusive facet value");
+    invalid_messages_.push_back("must be greater than or equal to minInclusive facet value");
+
+    // Initialize functors
+    print_functor_ = [](const std::string& xml_file, const std::string& profile_id, int32_t index) -> std::string
+            {
+                return default_external_unicast_locators::print_mask(xml_file, profile_id, index);
+            };
+    push_functor_ = [](const std::string& xml_file, const std::string& profile_id, const std::string& kind) -> void
+            {
+                default_external_unicast_locators::push_mask(xml_file, profile_id, kind);
+            };
+    update_functor_ = [](const std::string& xml_file, const std::string& profile_id, const std::string& kind,
+                    int32_t index) -> void
+            {
+                default_external_unicast_locators::update_mask(xml_file, profile_id, kind, index);
+            };
+    clear_functor_ = [](const std::string& xml_file, const std::string& profile_id, int32_t index) -> void
+            {
+                default_external_unicast_locators::clear_mask(xml_file, profile_id, index);
+            };
+
+    // Call test
+    print_push_update_clear_test();
 }
 
 int main(
