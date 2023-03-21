40cb34bc827fa043603abdce4fddfc3928e9098a
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2023-03-21 11:06:27 +0100

    Refs #17505: basic CLI parser: output version and basic level help
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

--- cli/src/cpp/fastdds_qos_profiles_manager_cli.cpp
+++ cli/src/cpp/fastdds_qos_profiles_manager_cli.cpp
@@ -12,9 +12,45 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <iostream>
+#include <map>
+#include <regex>
+
+#include <docopt/docopt.h>
+
+#include "config.h"
+
+static const char USAGE[] =
+        R"(Fast DDS QoS Profiles Manager CLI
+
+    Usage:
+      fastddsqosprof <file> <verb> [<args>...]
+      fastddsqosprof (-v | --version | -h | --help)
+
+    Options:
+      -v --version      CLI version.
+      -h --help         CLI general options.
+
+    The allowed <verb> commands are:
+      set               Write XML configuration parameter to file.
+      validate          Validate XML configuration file against Fast DDS XSD schema.
+      print             Print XML configuration parameter.
+      query             Query about XML parameter list size or keys.
+      clear             Erase XML configuration parameter from file.
+      compare           Compare between two XML configuration files.
+      help              CLI specific options for the corresponding configuration parameter.
+)";
+
 int main(
         int argc,
         char* argv[])
 {
+    std::map<std::string, docopt::value> args = docopt::docopt(
+        USAGE,
+        {argv + 1, argv + argc },
+        true,                                                       // show help if requested
+        FASTDDS_QOS_PROFILES_MANAGER_CLI_VERSION_STR,               // version string
+        true);                                                      // options first
+
     return 0;
 }
