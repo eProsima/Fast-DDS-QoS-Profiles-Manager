b4d9846df6e2a9a3282ce505f21898ae60a2ff9d
JesusPoderoso <jesuspoderoso@eprosima.com>
2023-04-03 11:23:45 +0200

    Refs #17501: Absolute path function impl
    Signed-off-by: JesusPoderoso <jesuspoderoso@eprosima.com>

---
+++ lib/src/cpp/utils/ParseXML.cpp
@@ -0,0 +1,53 @@
+// Copyright 2023 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file ParseXML.cpp
+ */
+
+#include <utils/ParseXML.hpp>
+
+#include <unistd.h>
+
+std::string get_absolute_path(
+    const std::string& xml_file)
+{
+    std::string absolute_xml_file;
+
+    //   Check if file already exists
+    char * absolute_xml_path = realpath(xml_file.c_str(), NULL);
+    //   File does not exist
+    if (absolute_xml_path == NULL)
+    {
+        // Check if it not already an absolute path
+        if (xml_file.find("/") == std::string::npos)
+        {
+            // Get current path and concat xml file name
+            absolute_xml_path = realpath(".", NULL);
+            absolute_xml_file = absolute_xml_path;
+            absolute_xml_file += "/" + xml_file;
+        }
+        // Set the given absolute path
+        else
+        {
+            absolute_xml_file = xml_file;
+        }
+    }
+    //   File exists: load file name directly
+    else
+    {
+        absolute_xml_file = absolute_xml_path;
+    }
+    return absolute_xml_file;
+}
