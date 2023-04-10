1de2063d5fcef44468a28af664f89c2434eda002
Jesús Poderoso <120394830+JesusPoderoso@users.noreply.github.com>
2023-04-10 11:17:49 +0200

    Add common locator list empty API (#32)
    Signed-off-by: JesusPoderoso <jesuspoderoso@eprosima.com>

---
+++ lib/src/cpp/common/LocatorList.cpp
@@ -0,0 +1,178 @@
+// Copyright 2023 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file
+ */
+
+#include <common/LocatorList.hpp>
+
+#include <string>
+
+#include <fastdds_qos_profiles_manager/exception/Exception.hpp>
+
+namespace eprosima {
+namespace qosprof {
+namespace common {
+namespace locator_list {
+
+std::string print(
+        xercesc::DOMElement*& xml_node,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_kind(
+        xercesc::DOMElement*& xml_node,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_port(
+        xercesc::DOMElement*& xml_node,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_physical_port(
+        xercesc::DOMElement*& xml_node,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_address(
+        xercesc::DOMElement*& xml_node,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_unique_lan_id(
+        xercesc::DOMElement*& xml_node,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+std::string print_wan_address(
+        xercesc::DOMElement*& xml_node,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+uint32_t size(
+        xercesc::DOMElement*& xml_node)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear(
+        xercesc::DOMElement*& xml_node,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_port(
+        xercesc::DOMElement*& xml_node,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_physical_port(
+        xercesc::DOMElement*& xml_node,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_address(
+        xercesc::DOMElement*& xml_node,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_unique_lan_id(
+        xercesc::DOMElement*& xml_node,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+void clear_wan_address(
+        xercesc::DOMElement*& xml_node,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_kind(
+        xercesc::DOMElement*& xml_node,
+        const std::string& kind,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_port(
+        xercesc::DOMElement*& xml_node,
+        const std::string& port,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_physical_port(
+        xercesc::DOMElement*& xml_node,
+        const std::string& physical_port,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_address(
+        xercesc::DOMElement*& xml_node,
+        const std::string& address,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_unique_lan_id(
+        xercesc::DOMElement*& xml_node,
+        const std::string& unique_lan_id,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+void set_wan_address(
+        xercesc::DOMElement*& xml_node,
+        const std::string& wan_address,
+        const std::string& index)
+{
+    throw Unsupported("Unsupported");
+}
+
+} // locator_list
+} // common
+} // qosprof
+} // eprosima
