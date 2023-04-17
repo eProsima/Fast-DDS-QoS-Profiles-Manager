38466593b6e074c8ab6dd53f934ae771d133915e
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2023-04-17 10:23:23 +0200

    Refs #18138: qos parser implementation
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

--- cli/src/cpp/utils/qos_parser.cpp
+++ cli/src/cpp/utils/qos_parser.cpp
@@ -15,6 +15,8 @@
 #include <string>
 #include <vector>
 
+#include <parser_constants.hpp>
+#include <usages.hpp>
 #include <utils/utils.hpp>
 
 namespace eprosima {
@@ -28,6 +30,112 @@ void qos_parser(
         std::string& element,
         const std::vector<std::string>& values)
 {
+    bool print_usage = false;
+    std::string subelement;
+    std::string key;
+
+    bool keyed = extract_element_subelement_key(element, subelement, key);
+
+    // Initialize message in case of error
+    std::ostringstream message;
+    message << ((entity == DDSEntity::DATAREADER) ? "DataReader <" : "DataWriter <") << element << "> QoS";
+
+    if (element == DATASHARING_ELEMENT)
+    {
+        std::cout << "DataSharing QoS configuration not yet supported" << std::endl;
+    }
+    else if (element == DURATION_SUBELEMENT)
+    {
+        std::cout << "Duration-related QoS (Deadline, Latency budget and Lifespan) configuration not yet supported"
+                  << std::endl;
+    }
+    else if (element == DISABLE_HEARTBEAT_PIGGYBACK_ELEMENT)
+    {
+        if (entity == DDSEntity::DATAREADER)
+        {
+            print_usage = true;
+            std::cout << message.str() << " not recognized" << std::endl;
+        }
+        else
+        {
+            std::cout << "Disable heartbeat piggyback QoS configuration not yet supported" << std::endl;
+        }
+    }
+    else if (element == DISABLE_POSITIVE_ACKS_ELEMENT)
+    {
+        std::cout << "Disable positive ACKs QoS Policy configuration not yet supported" << std::endl;
+    }
+    else if (element == DURABILITY_ELEMENT)
+    {
+        std::cout << "Durability QoS configuration not yet supported" << std::endl;
+    }
+    else if (element == GROUP_DATA_ELEMENT)
+    {
+        std::cout << "Group Data QoS Policy configuration not yet supported" << std::endl;
+    }
+    else if (element == HISTORY_ELEMENT)
+    {
+        std::cout << "History QoS Policy configuration not yet supported" << std::endl;
+    }
+    else if (element == LIVELINESS_ELEMENT)
+    {
+        std::cout << "Liveliness QoS Policy configuration not yet supported" << std::endl;
+    }
+    else if (element == OWNERSHIP_ELEMENT)
+    {
+        std::cout << "Ownership and Ownership strength QoS Policies configuration not yet supported" << std::endl;
+    }
+    else if (element == PARTITIONS_ELEMENT)
+    {
+        std::cout << "Partitions QoS Policy not yet supported" << std::endl;
+    }
+    else if (element == PUBLISH_MODE_ELEMENT)
+    {
+        if (entity == DDSEntity::DATAREADER)
+        {
+            print_usage = true;
+            std::cout << message.str() << " not recognized" << std::endl;
+        }
+        else
+        {
+            std::cout << "Publish mode QoS configuration not yet supported" << std::endl;
+        }
+    }
+    else if (element == RELIABILITY_ELEMENT)
+    {
+        std::cout << "Reliability QoS configuration not yet supported" << std::endl;
+    }
+    else if (element == RESOURCE_LIMITS_ELEMENT)
+    {
+        std::cout << "Resource Limits QoS configuration not yet supported" << std::endl;
+    }
+    else if (element == TOPIC_DATA_ELEMENT)
+    {
+        std::cout << "Topic Data QoS configuration not yet supported" << std::endl;
+    }
+    else if (element == USER_DATA_SUBELEMENT)
+    {
+        std::cout << "User Data QoS configuration not yet supported" << std::endl;
+    }
+    else
+    {
+        print_usage = true;
+        std::cout << message.str() << "not recognized" << std::endl;
+    }
+
+    if (print_usage)
+    {
+        if (CommonCommands::QUERY != command)
+        {
+            std::cout << ((entity == DDSEntity::DATAREADER) ? DATAREADER_QOS_USAGE : DATAWRITER_QOS_USAGE) << std::endl;
+        }
+        else
+        {
+            // TODO
+            // std::cout << ((entity == DDSEntity::DATAREADER) ? DATAREADER_QOS_QUERY_USAGE :
+            //        DATAWRITER_QOS_QUERY_USAGE) << std::endl;
+        }
+    }
 }
 
 } // qosprof_cli
